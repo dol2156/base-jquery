252b1160404e769a0e5e75fb64ea08d30d3f26ec
강석민 <dol2156@gmail.com>
2024-02-02 19:52:25 +0900

    ddd

---
+++ five-server.js
@@ -0,0 +1,17 @@
+// https://github.com/yandeu/five-server#readme
+// https://github.com/yandeu/five-server/blob/main/src/types.ts
+const FiveServer = require('five-server').default;
+const fiveServer = new FiveServer();
+const option = {
+  useLocalIp: true,
+  // host :'localhost',
+  port: 3000,
+  open: true,
+  watch: ['./page'],
+  // wait:1000,
+  // injectCss:false,
+  // ignore: [/\.s[ac]ss$/i, /\.tsx?$/i],
+  // ignore: ['./dist'],
+};
+
+fiveServer.start(option);
